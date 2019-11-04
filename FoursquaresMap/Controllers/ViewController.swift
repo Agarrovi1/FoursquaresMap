6c6c0fdddbd9e8fe7b99b6264fdfca1d4b046408
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-11-04 14:46:55 -0500

    constrained search bar and mapView

--- FoursquaresMap/Controllers/ViewController.swift
+++ FoursquaresMap/Controllers/ViewController.swift
@@ -7,17 +7,81 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
     
+    //MARK: - Objects
+    var querySearchBar = UISearchBar()
+    var mapView = MKMapView()
     
+    //MARK: - Properties
+    private var locationManager = CLLocationManager()
+    let initialLocation = CLLocation(latitude: 40.742054, longitude: -73.769417)
+    let searchRadius: CLLocationDistance = 2000
+    
+    //MARK: - Functions
+    private func locationAuthorization() {
+        let status = CLLocationManager.authorizationStatus()
+        switch status {
+        case .authorizedWhenInUse,.authorizedAlways:
+            mapView.showsUserLocation = true
+            locationManager.requestLocation()
+            locationManager.startUpdatingLocation()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            mapView.userTrackingMode = .follow
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    private func setDelegates() {
+        locationManager.delegate = self
+        mapView.delegate = self
+        querySearchBar.delegate = self
+    }
+    //MARK: - Constraints
+    private func setViewControllerUI() {
+        view.backgroundColor = .white
+        setSearchBarConstraints()
+        setMapViewConstraints()
+    }
+    private func setSearchBarConstraints() {
+        view.addSubview(querySearchBar)
+        querySearchBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            querySearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            querySearchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            querySearchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)])
+    }
+    private func setMapViewConstraints() {
+        view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: querySearchBar.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: querySearchBar.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: querySearchBar.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)])
+    }
 
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setViewControllerUI()
+        setDelegates()
+        locationAuthorization()
+        
     }
-
-
 }
 
+extension ViewController: UISearchBarDelegate {
+    
+}
+
+extension ViewController: MKMapViewDelegate {
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+}
