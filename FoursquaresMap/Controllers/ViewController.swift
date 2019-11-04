11bc06bd7d05c1724bc1dc633eb573c7b21fbf28
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-11-04 14:15:16 -0500

    adds tabBarController in sceneDelegate

--- FoursquaresMap/Controllers/ViewController.swift
+++ FoursquaresMap/Controllers/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
