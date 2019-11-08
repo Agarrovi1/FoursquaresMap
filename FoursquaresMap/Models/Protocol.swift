91ef4a7c9f10a20a6d827529dde3d2eb469d096d
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2019-11-08 15:11:00 -0500

    adds tests

--- FoursquaresMap/Models/Protocol.swift
+++ FoursquaresMap/Models/Protocol.swift
@@ -10,3 +10,5 @@ import Foundation
 protocol Reload {
     func reloadCollectionView()
 }
+
+
--- FoursquaresMapTests/FoursquaresMapTests.swift
+++ FoursquaresMapTests/FoursquaresMapTests.swift
@@ -31,4 +31,26 @@ class FoursquaresMapTests: XCTestCase {
         }
     }
 
+    func testVenueStruct() {
+        let testVenue = FourSquareVenues(response: ResponseWrapper(venues: [Venues(id: "58cc9644739d8523a63d716a", name: "Allegro Coffee Company", location: Location(address: "1095 Avenue Of The Americas", lat: 40.75453645610046, lng: -73.98430530273227, crossStreet: "41st"))]))
+        XCTAssertTrue(testVenue.response.venues[0].id == "58cc9644739d8523a63d716a")
+        XCTAssertTrue(testVenue.response.venues[0].name == "Allegro Coffee Company")
+        XCTAssertTrue(testVenue.response.venues[0].location.address == "1095 Avenue Of The Americas")
+        XCTAssertTrue(testVenue.response.venues[0].location.lat == 40.75453645610046)
+        XCTAssertTrue(testVenue.response.venues[0].location.lng == -73.98430530273227)
+        XCTAssertTrue(testVenue.response.venues[0].location.crossStreet == "41st")
+    }
+    func testCollectionsStruct() {
+        let testCollections = Collections(title: "TestOne", tip: nil, venues: [])
+        XCTAssertTrue(testCollections.title == "TestOne")
+        XCTAssertNil(testCollections.tip)
+        XCTAssertTrue(testCollections.venues.count == 0)
+    }
+    func testTipInCollections() {
+        let testCollections = Collections(title: "TestOne", tip: "TipOne", venues: [])
+        XCTAssertTrue(testCollections.title == "TestOne")
+        XCTAssertNotNil(testCollections.tip)
+        XCTAssertTrue(testCollections.tip == "TipOne")
+        XCTAssertTrue(testCollections.venues.count == 0)
+    }
 }
